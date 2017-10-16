59124456d5fd00bd45dbd3591126f515dfb5ad85
Taha Di Nero <taha1967@gmail.com>
2017-10-16 10:20:41 +0200

    mail cron

---
+++ Coverage/Quartz/JobRepository.cs
@@ -0,0 +1,24 @@
+﻿using System.Linq;
+using System.Collections.Generic;
+
+using Microsoft.EntityFrameworkCore;
+
+using Seac.Coverage.Models;
+using Seac.Coverage.Enum;
+
+namespace Seac.Coverage.Quartz
+{
+    public class JobRepository : IJobRepository
+    {
+        public JobDomainContext Context { get; set; }
+
+        public JobRepository(JobDomainContext context)
+        {
+            Context = context;
+        }
+
+        public IEnumerable<Leave> GetNotApprovedLeaves() => Context.Set<Leave>().AsNoTracking().Where(l => l.State != LeaveState.Approved).OrderBy(l => l.Date).ToList();
+
+        public IEnumerable<Employe> GetManagers() => Context.Set<Employe>().Where(e => e.Profile == EmployeProfile.Manager).ToList();
+    }
+}
---
+++ Coverage/Quartz/NotificationsJob.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Net.Mail;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Configuration;
+
+using Quartz;
+using Seac.Coverage.Mail;
+using Seac.Coverage.Models;
+
+using static Seac.Coverage.Utils.ConfigurationKeys;
+using static Seac.Coverage.Mail.MailManager;
+using Seac.Coverage.Enum;
+
+namespace Seac.Coverage.Quartz
+{
+    public class NotificationsJob : IJob
+    {
+        private readonly ILogger<NotificationsJob> _log;
+        private readonly IConfiguration _configuration;
+
+        private readonly IJobRepository _jobRepository;
+
+        public NotificationsJob(ILogger<NotificationsJob> log, IJobRepository jobRepository)
+        {
+            _log = log;
+            _configuration = new ConfigurationBuilder().SetBasePath(Directory.GetCurrentDirectory()).AddJsonFile(ServerConfFileName).Build();
+            _jobRepository = jobRepository;
+        }
+
+        public Task Execute(IJobExecutionContext context)
+        {
+            return Execute();
+        }
+
+        private async Task Execute()
+        {
+            try
+            {
+                var leaves = _jobRepository.GetNotApprovedLeaves();
+                if (leaves.Count() > 0)
+                {
+                    var managers = _jobRepository.GetManagers();
+                    var param = new ApprovationMailParams(NotificationType.Alert, GetServerUrl(), GetRecipients(managers));
+                    await SendMail(param);
+                }
+
+            }
+            catch (Exception ex)
+            {
+                _log.LogError(1, ex, "An error occurred during execution of notifications job");
+            }
+        }
+
+        private MailAddress[] GetRecipients(IEnumerable<Employe> recipients)
+        {
+            return recipients.Select(recipient => new MailAddress(recipient.Email, string.Concat(recipient.Surname, " ", recipient.Name))).ToArray();
+        }
+
+        private string GetServerUrl() => string.Format("http://{0}:{1}", _configuration[ServerName], _configuration[ServerHttpPort]);
+    }
+}
