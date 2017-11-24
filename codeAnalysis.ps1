e12a861380efd84f902a497009cc8cfdb411ea5a
Taha Di Nero <taha1967@gmail.com>
2017-11-24 11:03:51 +0100

    Update codeAnalysis.ps1

--- codeAnalysis.ps1
+++ codeAnalysis.ps1
@@ -1,3 +1,5 @@
-%1/SonarQube.Scanner.MSBuild.exe begin /k:"coverage" /d:sonar.host.url="http://localhost:9000" /d:sonar.login="f410b0383635c1a35a0e0c705ad84f99b2afebaa"
+Set-ExecutionPolicy RemoteSigned
+
+SonarQube.Scanner.MSBuild.exe begin /k:"coverage" /d:sonar.host.url="http://localhost:9000" /d:sonar.login="f410b0383635c1a35a0e0c705ad84f99b2afebaa"
 dotnet msbuild
-%1/SonarQube.Scanner.MSBuild.exe end /d:sonar.login="f410b0383635c1a35a0e0c705ad84f99b2afebaa"
+SonarQube.Scanner.MSBuild.exe end /d:sonar.login="f410b0383635c1a35a0e0c705ad84f99b2afebaa"
